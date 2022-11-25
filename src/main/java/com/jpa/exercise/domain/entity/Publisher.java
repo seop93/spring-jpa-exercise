ebce8ca36e2e0a4d3764a67712fa9c822db2554f
byeonheungseob <kevin9160@naver.com>
2022-11-25 14:06:45 +0900

    테이블 수정

--- src/main/java/com/jpa/exercise/domain/entity/Publisher.java
+++ src/main/java/com/jpa/exercise/domain/entity/Publisher.java
@@ -1,4 +1,21 @@
 package com.jpa.exercise.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Publisher {
+
+    @Id
+    private Long id;
+    private String name;
+    private String address;
+
 }
